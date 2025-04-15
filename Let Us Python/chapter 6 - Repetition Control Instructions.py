aa52fc0107d823a25b8629efbbd99d61bf627a8f
rishabhshukla <rishabh.shukla8@gmail.com>
2025-04-15 17:54:37 +0530

    added the repetition instruction files

---
+++ Let Us Python/chapter 6 - Repetition Control Instructions.py
@@ -0,0 +1,19 @@
+
+from common.common_library import clear_terminal
+
+"""
+Repetition Control Instructions 
+    - It helps in reapeating a set of instrcutions in a program 
+        - while 
+        - for 
+
+while loop 
+    - while loop is used to repeat set of instructions as long as condition is true 
+    - else block is optional , it is executes when condition fails 
+    - If loop is terminated abruptly , else block won't be executed 
+
+for loop 
+    - for is used iterate over elements of a sequence such as list, strings, tuple or set 
+
+ 
+"""
